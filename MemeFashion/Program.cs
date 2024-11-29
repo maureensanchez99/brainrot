6cbb278b9c1c12d3df1277bfff5c73cc5401acd0
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2024-11-29 10:36:27 -0600

    adjusting files

--- MemeFashion/Program.cs
+++ MemeFashion/Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<OutfitGeneratorService>();
 // Add this line with the other service registrations
 builder.Services.AddScoped<MemeStateService>();
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddAntiforgery();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<OutfitGeneratorService>();
 
 var app = builder.Build();
 
@@ -18,6 +24,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
 
@@ -33,5 +45,15 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAntiforgery();
+app.UseAuthorization();
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
 
 app.Run();
